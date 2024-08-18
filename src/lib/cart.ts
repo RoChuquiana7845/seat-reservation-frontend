f09ae247311c6805000735658fb28bf81f8d369d
Anthony Terán <aterana3@unemi.edu.ec>
2024-08-18 11:22:30 -0500

    conexiones del carrito

---
+++ src/lib/cart.ts
@@ -0,0 +1,73 @@
+import api from "./api";
+import { AxiosError } from "axios";
+
+export const createCart = async(userId : string) => {
+    try {
+        const response = await api.post("api/cart", {
+            id: userId
+        });
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response) {
+            throw error.response.data;
+        } else {
+            throw new Error("An unexpected error occurred");
+        }
+    }
+}
+
+export const getCart = async(userId : string) => {
+    try {
+        const response = await api.get(`api/cart/${userId}`);
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response) {
+            throw error.response.data;
+        } else {
+            throw new Error("An unexpected error occurred");
+        }
+    }
+}
+
+export const addItemToCart = async(quantity: number, cartId: string, productId: string) => {
+    try {
+        const response = await api.post("api/cart-items", {
+            quantity,
+            cart: cartId,
+            product: productId
+        });
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response) {
+            throw error.response.data;
+        } else {
+            throw new Error("An unexpected error occurred");
+        }
+    }
+}
+
+export const removeItemFromCart = async(productId: string) => {
+    try {
+        const response = await api.delete(`api/cart-items/${productId}`);
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response) {
+            throw error.response.data;
+        } else {
+            throw new Error("An unexpected error occurred");
+        }
+    }
+}
+
+export const getCartItems = async(userId: string) => {
+    try {
+        const response = await api.get(`api/cart/${userId}/items`);
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response) {
+            throw error.response.data;
+        } else {
+            throw new Error("An unexpected error occurred");
+        }
+    }
+}
