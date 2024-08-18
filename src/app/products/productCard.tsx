87b70b0a5eb65aa67d9f69679e6b6aff6a016e88
Anthony Terán <aterana3@unemi.edu.ec>
2024-08-17 20:01:36 -0500

    Cambios en el diseño de lista de productos

--- src/app/products/productCard.tsx
+++ src/app/products/productCard.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 import { ProductProps } from "@/types/index";
+
 
 export default function ProductCard({product} : {product: ProductProps}) {
     return (
         <div className="border border-gray-300 rounded-lg p-5 text-center shadow-md transition-transform transform hover:-translate-y-1 hover:shadow-lg">
+            <Image 
+                className="w-full h-auto max-h-40 mb-3 object-contain" 
+                src="/picture/default-product.png" 
+                alt={`Imagen de ${product.name}`}
+                width={300}
+                height={300}
+            />
             <div className="text-left">
                 <h3 className="text-lg font-semibold">{product.name}</h3>
                 <p className="text-sm text-gray-500">${product.precio}</p>
