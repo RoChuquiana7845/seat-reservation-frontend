d92d03b7ba70185b1f4e8bb65f55976565da1d43
RoChuquiana7845 <rchuquianat@unemi.edu.ec>
2024-08-18 18:27:48 -0500

    solved check status bug (front end) v0.3

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,41 +1,6 @@
 import api from "./api";
 import { AxiosError } from "axios";
 
-// Función para registrarse (Sign Up)
-export const signup = async (userData: {
-  username: string;
-  email: string;
-  password: string;
-}) => {
-  try {
-    const response = await api.post("/api/auth/signup", userData);
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError && error.response) {
-      throw error.response.data;
-    } else {
-      throw new Error("An unexpected error occurred");
-    }
-  }
-};
-
-// Función para iniciar sesión (Login)
-export const login = async (credentials: {
-  email: string;
-  password: string;
-}) => {
-  try {
-    const response = await api.post("/api/auth/login", credentials);
-    return response.data;
-  } catch (error) {
-    if (error instanceof AxiosError && error.response) {
-      throw error.response.data;
-    } else {
-      throw new Error("An unexpected error occurred");
-    }
-  }
-};
-
 // Función para verificar el estado de autenticación (Check Auth Status)
 export const checkAuthStatus = async () => {
   try {
@@ -45,29 +10,15 @@ export const checkAuthStatus = async () => {
     }
     return false; // En caso de que la respuesta no sea como se espera
   } catch (error) {
-    if (error instanceof AxiosError && error.response) {
-      if (error.response.status === 401) {
+    const axiosError = error as AxiosError; // Aquí usamos una type assertion
+    if (axiosError.response) {
+      if (axiosError.response.status === 401) {
         return false; // El usuario no está autenticado
       }
-      console.error('Error response:', error.response.data);
-      throw error.response.data;
+      console.error("Error response:", axiosError.response.data);
+      throw axiosError.response.data;
     } else {
-      console.error('Unexpected error:', error.message);
-      throw new Error("An unexpected error occurred");
-    }
-  }
-};
-
-
-// Función para cerrar sesión (Logout)
-export const logout = async () => {
-  try {
-    await api.post("/auth/logout");
-    window.location.href = "/auth/login"; // Redirige al usuario al login después de cerrar sesión
-  } catch (error) {
-    if (error instanceof AxiosError && error.response) {
-      throw error.response.data;
-    } else {
+      console.error("Unexpected error:", axiosError.message);
       throw new Error("An unexpected error occurred");
     }
   }
