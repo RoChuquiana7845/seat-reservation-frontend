dd776f5134d556dc70d631d37af66f54ba3d9762
RoChuquiana7845 <rchuquianat@unemi.edu.ec>
2024-08-19 17:14:21 -0500

    solved check status bug (front end) v0.5

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,28 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
+// Crear la instancia de Axios
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3004/',
   withCredentials: true, 
 });
 
+// Función para obtener el token JWT de las cookies
+export const getJwtToken = async (): Promise<string | undefined> => {
+  if (typeof window !== 'undefined') {
+    const token = document.cookie;
+    console.log('Token:', token); // Debería mostrar el valor de la cookie o `undefined`
+  }
+  return await Cookies.get('jwt'); // Devuelve el valor de la cookie 'jwt', o undefined si no está presente
+};
+
 // Interceptores de solicitud
 api.interceptors.request.use(
   (config) => {
+    const token = getJwtToken(); // Obtener el token JWT
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // Agregar el token JWT a la cabecera Authorization
+    }
     return config;
   },
   (error) => {
@@ -23,7 +38,7 @@ api.interceptors.response.use(
   (error) => {
     // Manejar errores de manera centralizada
     if (error.response && error.response.status === 401) {
-      return null;
+      console.error('No autorizado:', error.response.data);
     }
     return Promise.reject(error);
   }
