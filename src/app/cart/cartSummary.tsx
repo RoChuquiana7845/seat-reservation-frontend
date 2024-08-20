62a115b6924520b8c272ed8d91659c05ec8be783
Anthony Terán <aterana3@unemi.edu.ec>
2024-08-19 22:00:08 -0500

    Cambios en el diseño del carro

--- src/app/cart/cartSummary.tsx
+++ src/app/cart/cartSummary.tsx
@@ -1,11 +1,39 @@
-import { useAuth } from "@/hooks/useAuth";
+"use client";
+
+import { useCart } from "@/hooks/useCart";
+import { useEffect, useState } from "react";
+import {getCartItem} from "@/lib/cart";
 
 export default function CartSummary() {
+    const { cart, loading } = useCart();
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        if (cart && cart.id) {
+            const calculateTotal = async () => {
+                const total = await Promise.all(
+                    cart.cartItems.map(async (item) => {
+                        const data = await getCartItem(item.id);
+                        const price = parseFloat(data.product.precio);
+                        return price * item.quantity;
+                    })
+                );
+                const sum = total.reduce((acc, item) => acc + item, 0);
+                setTotal(sum);
+            };
+            calculateTotal();
+        }
+    }, [cart]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <h2 className="text-2xl font-semibold">Resumen del carrito</h2>
-            <p>Total: $100</p>
+            <p>Total: ${total}</p>
             <button className="bg-blue-500 text-white px-4 py-2 rounded">Pagar</button>
         </div>
-    )
+    );
 }
